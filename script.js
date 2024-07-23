bf689d5b78c4bb23194a9e9da581a28a4225347b
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-07-23 13:30:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-// script.js
+// // script.js
 
-document.getElementById('testingJSbutton').addEventListener('click', async () => {
-    const response = await fetch('/api/text');
+// document.getElementById('testingJSbutton').addEventListener('click', async () => {
+//     const response = await fetch('/api/text');
 
-    if (response.ok) {
-        const data = await response.json();
-        document.getElementById('outputText').innerText = data.text;
+//     if (response.ok) {
+//         const data = await response.json();
+//         document.getElementById('outputText').innerText = data.text;
+//     } else {
+//         alert('Failed to retrieve text');
+//     }
+// });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    const button = document.getElementById('testingJSbutton');
+    const output = document.getElementById('outputText');
+
+    const rgbHighSlider = document.getElementById('rgbHighSlider');
+    const rgbLowSlider = document.getElementById('rgbLowSlider');
+
+    if (button) {
+        console.log('Button found');
+
+        button.addEventListener('click', async () => {
+            console.log('Button clicked');
+
+            const rgbHighValue = parseInt(rgbHighSlider.value);
+            const rgbLowValue = parseInt(rgbLowSlider.value);
+
+            try {
+                const response = await fetch(`/api/text?rgbHigh=${rgbHighValue}&rgbLow=${rgbLowValue}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    output.innerText = data.text;
+                } else {
+                    const errorData = await response.json();
+                    console.error('Failed to retrieve text', response.status, errorData.message);
+                    output.innerText = errorData.message;
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        });
     } else {
-        alert('Failed to retrieve text');
+        console.error('Button not found');
     }
 });
