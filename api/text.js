8e7cb66b007c88ce4ffbfa1907e372e3535993ce
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-07-23 13:30:04 +0300

    Update text.js

--- api/text.js
+++ api/text.js
@@ -1,12 +1,27 @@
-// Testing first file text.js
+// // Testing first file text.js
 
-// api/text.js
+// // api/text.js
 
-const storedText = 'JJ -- This is the manually set text you want to retrieve.';
+// const storedText = 'JJ -- This is the manually set text you want to retrieve.';
+
+// export default function handler(req, res) {
+//   if (req.method === 'GET') {
+//     res.status(200).json({ text: storedText });
+//   } else {
+//     res.status(405).json({ message: 'Method not allowed' });
+//   }
+// }
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
-    res.status(200).json({ text: storedText });
+    const { rgbHigh, rgbLow } = req.query;
+
+    if (rgbHigh === '250' && rgbLow === '205') {
+      const storedText = 'This is the manually set text you want to retrieve.';
+      res.status(200).json({ text: storedText });
+    } else {
+      res.status(403).json({ message: 'Forbidden: Incorrect RGB values' });
+    }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
   }
