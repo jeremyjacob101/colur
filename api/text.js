7d22003b480764b4ded2ab5a69b47b5f279f8edd
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-07-23 12:53:06 +0300

    Update text.js

--- api/text.js
+++ api/text.js
@@ -1,1 +1,13 @@
 // Testing first file text.js
+
+// api/text.js
+
+const storedText = ' JJ -- This is the manually set text you want to retrieve.';
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json({ text: storedText });
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
