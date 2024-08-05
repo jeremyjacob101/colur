249740efd0c6939e49477a7dcf79a1bf17db60cc
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-08-05 16:25:11 +0300

    Update text.js

--- api/text.js
+++ api/text.js
@@ -5,14 +5,13 @@ export default function handler(req, res) {
     if (rgbHigh === '250' && rgbLow === '205') {
       const storedText = '--key--';
       res.status(200).json({ text: storedText });
-    } else {
-      res.status(403).json({ message: 'KEY - Incorrect RGB values' });
     }
-    if (rgbHigh === '175' && rgbLow === '70') {
+    else if (rgbHigh === '175' && rgbLow === '70') {
       const storedText = '--offset--';
       res.status(200).json({ text: storedText });
-    } else {
-      res.status(403).json({ message: 'OFFSET - Incorrect RGB values' });
+    }
+    else {
+      res.status(403).json({ message: 'Incorrect RGB values' });
     }
   } else {
     res.status(405).json({ message: 'Method not allowed' });
