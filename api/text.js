97d392e625e9cde9535d579304c01e3b3dd38df6
jeremyjacob101 <152927755+jeremyjacob101@users.noreply.github.com>
2024-08-05 16:17:29 +0300

    Update text.js

--- api/text.js
+++ api/text.js
@@ -3,7 +3,13 @@ export default function handler(req, res) {
     const { rgbHigh, rgbLow } = req.query;
 
     if (rgbHigh === '250' && rgbLow === '205') {
-      const storedText = 'JJ -- Hint here!';
+      const storedText = '--key--';
+      res.status(200).json({ text: storedText });
+    } else {
+      res.status(403).json({ message: 'Forbidden: Incorrect RGB values' });
+    }
+    if (rgbHigh === '70' && rgbLow === '175') {
+      const storedText = '--offset--';
       res.status(200).json({ text: storedText });
     } else {
       res.status(403).json({ message: 'Forbidden: Incorrect RGB values' });
